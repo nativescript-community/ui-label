7cf80486528cc96c0d780253b2d0484aa13cd162
Martin Guillon <martin.guillon@akylas.fr>
2019-10-16 16:23:28 +0200

    test: update demo app

--- demo-vue/app/examples/VerticalAlignment.vue
+++ demo-vue/app/examples/VerticalAlignment.vue
@@ -5,16 +5,16 @@
         </ActionBar>
         <ScrollView>
             <StackLayout>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="red" height="100" lineHeight="0" color="green" textWrap="true" text="left textAlignment, top verticalAlignment" verticalAlignment="top"/>
-            <HTMLLabel fontSize="10" width="100%" backgroundColor="green" height="100" color="black" textWrap="true" text="left textAlignment, center verticalAlignment" verticalAlignment="center"/>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="blue" height="100" color="black" textWrap="true" text="left textAlignment, bottom verticalAlignment" verticalAlignment="bottom"/>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="red" isUserInteractionEnabled="false" height="100" textAlignment="right" color="black" textWrap="false" text="right textAlignment, top verticalAlignment" verticalAlignment="top"/>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="green" height="100" color="black" textWrap="false" text="left textAlignment, center verticalAlignment" verticalAlignment="center"/>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="yellow" height="100" color="black" textWrap="false" verticalAlignment="center"> 
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="red" height="100" lineHeight="0" color="green" textWrap="true" text="left textAlignment, top verticalAlignment" verticalTextAlignment="top"/>
+            <HTMLLabel fontSize="10" width="100%" backgroundColor="green" height="100" color="black" textWrap="true" text="left textAlignment, center verticalAlignment" verticalTextAlignment="center"/>
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="blue" height="100" color="black" textWrap="true" text="left textAlignment, bottom verticalAlignment" verticalTextAlignment="bottom"/>
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="red" isUserInteractionEnabled="false" height="100" textAlignment="right" color="black" textWrap="false" text="right textAlignment, top verticalAlignment" verticalTextAlignment="top"/>
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="green" height="100" color="black" textWrap="false" text="left textAlignment, center verticalAlignment" verticalTextAlignment="center"/>
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="yellow" height="100" color="black" textWrap="false" verticalTextAlignment="center"> 
                 <Span text="left textAlignment"/>
                 <Span text=", center verticalAlignment" color="red" fontSize="20"/>
             </HTMLLabel>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="blue" height="100" textAlignment="center" color="black" textWrap="false" text="center textAlignment, bottom verticalAlignment" verticalAlignment="bottom"/>
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="blue" height="100" textAlignment="center" color="black" textWrap="false" text="center textAlignment, bottom verticalAlignment" verticalTextAlignment="bottom"/>
 
         </StackLayout>
         </ScrollView>
