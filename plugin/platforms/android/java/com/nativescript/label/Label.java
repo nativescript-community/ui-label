36badf821b4d0f4cbde45c85e5d18ab5bc588d72
Martin Guillon <martin.guillon@akylas.fr>
2020-02-11 15:19:58 +0100

    fix(android): starting android label rewrite
    Use native code as much as possible for better performances

---
+++ plugin/platforms/android/java/com/nativescript/label/Label.java
@@ -0,0 +1,64 @@
+package com.nativescript.label;
+
+import android.graphics.Typeface;
+import android.content.Context;
+import android.text.method.TransformationMethod;
+
+public class Label extends com.lsjwzh.widget.text.FastTextView {
+    public Label(Context context) {
+        super(context);
+    }
+
+    public TransformationMethod getTransformationMethod() {
+        return null;
+    }
+
+    public void setTransformationMethod(TransformationMethod value) {
+
+    }
+    public void setSingleLine(boolean value) {
+
+    }
+    public void setMinLines(int value) {
+
+    }
+    public void setMinHeight(int value) {
+
+    }
+    public void setMaxHeight(int value) {
+
+    }
+
+    public int getMinHeight() {
+        return 0;
+
+    }
+    public int getMinLines() {
+        return 0;
+
+    }
+    public int getMaxHeight() {
+        return 0;
+
+    }
+    public int getLineSpacingExtra() {
+        return 0;
+
+    }
+    public void setLineSpacingExtra(int value) {
+
+    }
+
+    public Typeface getTypeface() {
+        return getTextPaint().getTypeface();
+    }
+    public void setTypeface(Typeface typeface) {
+        getTextPaint().setTypeface(typeface);
+    }
+    public int getPaintFlags() {
+        return getTextPaint().getFlags();
+    }
+    public void setPaintFlags(int typeface) {
+        getTextPaint().setFlags(typeface);
+    }
+}
---
+++ src/typings/android.d.ts
@@ -0,0 +1,11 @@
+declare module com {
+    export module lsjwzh {
+        export module widget {
+            export module text {
+                export class FastTextView extends globalAndroid.view.View {
+
+                }
+            }
+        }
+    }
+}
