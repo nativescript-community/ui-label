661b9dc49214e9b70c7d23bf0e46f897062c4c82
Martin Guillon <martin.guillon@akylas.fr>
2020-05-28 15:26:40 +0200

    test: demo app

--- demo-vue/app/examples/VerticalAlignment.vue
+++ demo-vue/app/examples/VerticalAlignment.vue
@@ -5,7 +5,7 @@
         </ActionBar>
         <ScrollView>
             <StackLayout>
-            <HTMLLabel fontSize="20" width="100%" backgroundColor="red" height="100" lineHeight="0" color="green" textWrap="true" text="left textAlignment, top verticalAlignment" verticalTextAlignment="top"/>
+            <HTMLLabel fontSize="20" width="100%" backgroundColor="red" height="100" lineHeight="0" color="green" textWrap="true" html='<span style="color:blue;font-size:26;font-style:italic;">left textAlignment</span>, top verticalAlignment' verticalTextAlignment="top"/>
             <HTMLLabel fontSize="10" width="100%" backgroundColor="green" height="100" color="black" textWrap="true" text="left textAlignment, center verticalAlignment" verticalTextAlignment="center"/>
             <HTMLLabel fontSize="20" width="100%" backgroundColor="blue" height="100" color="black" textWrap="true" text="left textAlignment, bottom verticalAlignment" verticalTextAlignment="bottom"/>
             <HTMLLabel fontSize="20" width="100%" backgroundColor="red" isUserInteractionEnabled="false" height="100" textAlignment="right" color="black" textWrap="false" text="right textAlignment, top verticalAlignment" verticalTextAlignment="top"/>
