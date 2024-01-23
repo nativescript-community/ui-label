e4f4a4d5322dc92aebeee16b50913a8a9d9769bd
farfromrefuge <>
2024-01-23 11:01:49 +0100

    fix(android): faster and lighter(using proguard/native-api-usage) implementation

--- src/typings/android.d.ts
+++ src/typings/android.d.ts
@@ -1,7 +1,21 @@
 declare namespace com {
     export namespace nativescript {
         export namespace label {
-            export class EllipsizingTextView extends androidx.appcompat.widget.AppCompatTextView {}
+            export class NSLabel extends text.TextView {
+                static attributedStringHasURLSpan(attributeString: android.text.Spannable): boolean;
+                static inflateLayout(context: android.content.Context): NSLabel;
+
+                setTextDecoration(value: string | number);
+                setLineBreak(value: string);
+                setWhiteSpace(value: string);
+                setVerticalTextAlignment(value: string, textAlignment: string);
+                setLabelTextAlignment(value: string, verticalTextAlignment: string);
+
+                setLabelTextSize(unit: number, size: number, minFontSize: number, maxFontSize: number, step: number);
+                enableAutoSize(minFontSize: number, maxFontSize: number, step: number);
+                disableAutoSize();
+                setTappableState(value: boolean);
+            }
         }
     }
 }
