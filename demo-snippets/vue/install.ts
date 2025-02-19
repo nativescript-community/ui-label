45c317b9ec6eeed61437e4465c435072519875b7
farfromrefug <>
2025-02-19 09:38:45 +0100

    chore: demo fix

--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -1,6 +1,9 @@
 import Vue from 'nativescript-vue';
 import Basic from './Basic.vue';
 
-export function installPlugin() {}
+export function installPlugin() {
+    Vue.registerElement('HTMLLabel', () => require('@nativescript-community/ui-label').Label);
+
+}
 
 export const demos = [{ name: 'Basic', path: 'basic', component: Basic }];
